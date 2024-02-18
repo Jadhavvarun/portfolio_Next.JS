5d3c81d56d9f18fa953829e42b9a4f522c3aa1b3
Jadhavvarun <jadhavvarun67@gmail.com>
2024-02-18 23:15:34 +0530

    add ons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
-'use client'
+"use client"
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { MdArrowOutward } from 'react-icons/md';
+import { global } from 'styled-jsx/css';
 
 const Home = () => {
   const [textIndex, setTextIndex] = useState(0);
@@ -20,7 +21,33 @@ const Home = () => {
   }, [textVariants.length]);
 
   return (
-    <div>
+    <div className="h-auto">
+      {/* Introduction Section */}
+      <div className={`bg-blue-2 flex justify-center items-center py-20 min-h-screen`}>
+        <div className="container mx-auto px-4 text-center">
+          <h1 className="text-4xl md:text-5xl font-bold text-white-1 mb-4 md:text-left">
+            Welcome to my Portfolio!
+          </h1>
+          <div className="flex flex-col md:flex-row items-center justify-center mb-4 md:text-center">
+            <div className="w-full md:w-1/2">
+              <Image
+                src="/intro.webp"
+                alt="programmer"
+                width={500}
+                height={500}
+                priority
+                className=""
+              />
+            </div>
+            <div className="w-full md:w-1/2 md:text-left text-white-1 mt-4 md:mt-0">
+              <h1 className="text-6xl md:text-7xl font-bold mb-4">Hello There! 👋</h1>
+              <h2 className="text-5xl md:text-6xl font-bold mb-4">Varun Jadhav</h2>
+              <h3 className="text-2xl md:text-3xl font-semibold">Software Developer</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Main Content Section */}
       <div className="min-h-screen bg-white-1 flex flex-col relative">
         <div className="flex-1 flex flex-col md:flex-row">
           <div className="md:w-1/2 flex justify-center items-center mt-8">
